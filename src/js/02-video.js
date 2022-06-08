b26c709d8aaf09b44655ede5e913a09328211fb1
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2022-06-08 15:26:09 +0200

    bugfix vineo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+    const player = new Vimeo.Player(iframe);
 
 player.on("timeupdate", throttle(svTime, 1000))
 
