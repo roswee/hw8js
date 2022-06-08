97ee133479e97002ace1cc5e049b0198a888da12
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2022-06-08 12:21:49 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGallery() {
+    const imgs = [];
+    galleryItems.forEach(event => {
+      const { preview, original, description } = event;
+      const item = document.createElement("a");
+      item.classList.add("gallery__link");
+      item.href = `${original}`;
+      imgs.push(item);
+      const img = document.createElement("img");
+      img.classList.add("gallery__image");
+      img.src = `${preview}`;
+      img.dataset.source = `${original}`;
+      img.alt = `${description}`;
+      item.append(img);
+    });
+    gallery.append(...imgs);
+  }
+createGallery();
+
+gallery.addEventListener("click", galleryModal)
+
+function galleryModal(event) {
+    event.preventDefault();
+    if (event.target.tagName !== "IMG")
+    {return};
+    
+    const target = event.target.dataset.source;
+    event.target.src = target;
+    const instance = basicLightbox.create(`<img src=${target}>`)
+    instance.show(() => console.log(`Voilà!`))
+    
+}
